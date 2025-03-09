75e95afd63a5f87efc9116989f533fdb4d7d6f5c
Ichinose Yoshihiro <maruisansmai@gmail.com>
2025-03-09 23:33:51 +0900

    feat: create :feature:repositorydetail

---
+++ feature/repositorydetail/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hiltAndroid)
+    alias(libs.plugins.apollo)
+}
+
+android {
+    namespace = "com.github.mikan.ssot.sample.repositorydetail"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidxLifecycleViewModelCompose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    // di
+    implementation(libs.hiltAndroid)
+    ksp(libs.hiltCompiler)
+    // network
+    implementation(projects.core.network)
+    implementation(projects.core.designsystem)
+    // image
+    implementation(libs.coilCompose)
+    implementation(libs.coilNetworkOkhttp)
+}
+
+apollo {
+    service("github") {
+        packageName = "com.github.mikan.ssot.sample.repositorydetail"
+        dependsOn(projects.core.network)
+    }
+}
